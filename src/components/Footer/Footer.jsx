5e7d144bad7835e6a14d59f831671036047fb899
omar faruk <farukomarcpa@gmail.com>
2024-03-20 20:08:43 +0600

    react-route first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <h2><small>this is our website footer</small></h2>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,11 @@
+
+
+const Users = () => {
+    return (
+        <div>
+            <h2>This is for Users Page</h2>
+        </div>
+    );
+};
+
+export default Users;
